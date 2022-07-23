0f6f86cabccefa3614bde5e60edf75b9d15b163e
Mogakamo <mogaka.amo254@gmail.com>
2022-07-23 21:03:24 +0300

    fix: resolver import error

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
-const resolvers = require("./resolvers");
+const resolvers = require("./resolver");
 
 const TrackAPI = require("./datasources/track-api");
 
