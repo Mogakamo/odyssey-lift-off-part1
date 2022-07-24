2ead231aa698afbcf55f9eec39dbf0fd687ceacc
Mogakamo <mogaka.amo254@gmail.com>
2022-07-24 07:25:17 +0300

    finished with serverside mutation

--- server/src/resolver.js
+++ server/src/resolver.js
@@ -3,26 +3,48 @@ const resolvers = {
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     },
-    track: async (_, {id}, {dataSources}) => {
+    track: async (_, { id }, { dataSources }) => {
       // get track details
-        return dataSources.trackAPI.getTrack(id);
+      const track = await dataSources.trackAPI.getTrack(id);
 
-        // get module details for the track
-        const modules = await dataSources.trackAPI.getTrackModules(id);
 
-        // shape the data in the way the schema expects it
-        return {...track, modules}
+      // get module details for the track
+      const modules = await dataSources.trackAPI.getTrackModules(id);
+
+      // shape the data in the way the schema expects it
+      return { ...track, modules };
+    },
+  },
+
+  Mutation: {
+    incrementTrackViews: async (_, {id}, {dataSources}) => {
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+      return {
+        code: 200,
+        success: true,
+        message: `Successfully incremented track ${id}'s number of views`,
+        track
+      }
+      } catch (e) {
+        return {
+          code: e.extensions.response.status,
+          success: false,
+          message: e.extensions.response.body,
+          track: null
+        }
+      }
     }
   },
+
   Track: {
-    author: ({authorId}, _, { dataSources }) => {
+    author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
-    modules: ({id}, _, {dataSources}) => {
-      return dataSources.trackAPI.getTrackModules(id)
-    }
+    modules: ({ id }, _, { dataSources }) => {
+      return dataSources.trackAPI.getTrackModules(id);
+    },
   },
-
 };
 
 module.exports = resolvers;
