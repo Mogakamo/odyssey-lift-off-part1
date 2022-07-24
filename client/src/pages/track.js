1dd2245b23f9c0d941bab0dcf494f5eac81e4ffd
Mogakamo <mogaka.amo254@gmail.com>
2022-07-24 06:20:39 +0300

    /track/:trackId is working

--- client/src/pages/track.js
+++ client/src/pages/track.js
@@ -1,8 +1,41 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
-import { Layout } from "../components";
+import { Layout, QueryResult } from "../components";
+import TrackDetail from "../components/track-detail"
+
+export const GET_TRACK = gql`
+    query GetTrack($trackId: ID!) {
+        track(id: $trackId) {
+            id
+            title
+            author {
+                id
+                name
+                photo
+            }
+            thumbnail
+            length
+            modulesCount
+            numberOfViews
+            modules {
+                id
+                title
+                length
+            }
+            description
+        }
+    }
+`
 
 const Track = ({ trackId }) => {
-  return <Layout></Layout>;
+    const {loading, data, error} = useQuery(GET_TRACK, {
+        variables: { trackId }
+    })
+  return <Layout>
+    <QueryResult loading={loading} data={data} error={error}>
+        <TrackDetail track={data?.track} />
+    </QueryResult>
+  </Layout>;
 };
 
 export default Track;
