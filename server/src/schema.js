766c23032223b6c1121a5aa61492b7454703daed
Mogakamo <mogaka.amo254@gmail.com>
2022-07-24 06:51:55 +0300

    Learning mutations

--- server/src/schema.js
+++ server/src/schema.js
@@ -45,6 +45,21 @@ const typeDefs = gql`
     "The module's length in minutes"
     length: Int
   }
+
+
+  type Mutation {
+    incrementTrackViews(id: ID!): IncrementTrackViewsResponse!
+  }
+
+  type IncrementTrackViewsResponse {
+    "Similar to HTTP status code, represent the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human-readable message for the UI"
+    track: Track
+  }
 `;
+
 
 module.exports = typeDefs;
